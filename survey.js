819ad9db507ffbfd1833f5d4645133c643e26953
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-08 01:23:01 +0000

    Online Profile Generator

---
+++ survey.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? Nicknames are also acceptable ", (name) => {
+  rl.question("What's an activity you like doing? ", (activity) => {
+    rl.question("What do you listen to while doing that? ", (listen) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (meal) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (dish) => {
+          rl.question("Which sport is your absolute favourite? ", (sport) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (power) => {
+              console.log(`${name} loves listening to ${listen} while ${activity}, huge fan of ${dish} for ${meal}, has been playing ${sport} since 5 years of age, and is amazing at ${power}.`);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
